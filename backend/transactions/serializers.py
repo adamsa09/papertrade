e674d6af9ddea245cf2620969ce89c67131d5749
Adam Sarhan <adam_sar09@outlook.com>
2026-03-06 23:13:01 -0500

    Minor bug fix

--- backend/transactions/serializers.py
+++ backend/transactions/serializers.py
@@ -1,10 +1,8 @@
 from rest_framework import serializers
-<<<<<<< HEAD
-=======
 from .models import Trade
 
 class TradeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Trade
         fields = '__all__'
->>>>>>> 571f8e3ad5d5a6479d6294ded5b4c3ac94a3a70e
+
